a01d7dc1da50d1dc4ce321a77646483da8d3cc32
Umbra <69671761+Umbra999@users.noreply.github.com>
2024-09-13 19:14:48 +0200

    general udon blocking instead just fbt

--- VRC-AntiFBTHeaven/Boot.cs
+++ VRC-AntiFBTHeaven/Boot.cs
@@ -8,7 +8,7 @@ namespace VRC_AntiFBTHeaven
     {
         public static void Main()
         {
-            Console.Title = "ANTI FBT HEAVEN BY UMBRA";
+            Console.Title = Utils.RandomString(20);
 
             WindowsIdentity identity = WindowsIdentity.GetCurrent();
             WindowsPrincipal principal = new(identity);
@@ -27,120 +27,48 @@ namespace VRC_AntiFBTHeaven
                 return;
             }
 
-            ToggleHostBlock(false);
-
-            Task ListDumper = Task.Run(APIClient.FetchLists);
-            ListDumper.Wait();
-
-            ScanLog();
+            while (true)
+            {
+                ToggleHostBlock(false);
+                Console.ReadLine();
+                ToggleHostBlock(true);
+                Console.ReadLine();
+            }
         }
 
         private static void ToggleHostBlock(bool State)
         {
+            List<string> targetDomains = new()
+            {
+                "pastebin.com",
+                "github.io",
+                "gist.githubusercontent.com"
+            };
+
             string hostsFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "drivers/etc/hosts");
-            string targetDomain = "0.0.0.0 pastebin.com";
             List<string> savedFile = File.ReadAllLines(hostsFilePath).ToList();
 
             if (State)
             {
-                if (savedFile.Contains(targetDomain)) return;
-                savedFile.Add(targetDomain);
-                Logger.LogSuccess("Joining FBT Heaven, Anti enabled");
+                foreach (string domain in targetDomains)
+                {
+                    if (!savedFile.Contains(domain)) savedFile.Add(domain);
+                }
+
+                Logger.LogSuccess("UdonBlocker ENABLED [PRESS ENTER TO TOGGLE]");
             }
             else
             {
-                if (!savedFile.Contains(targetDomain)) return;
-                savedFile.Remove(targetDomain);
-                Logger.LogSuccess("Leaving FBT Heaven, Anti disabled");
+
+                foreach (string domain in targetDomains)
+                {
+                    if (savedFile.Contains(domain)) savedFile.Remove(domain);
+                }
+
+                Logger.LogError("UdonBlocker DISABLED [PRESS ENTER TO TOGGLE]");
             }
 
             File.WriteAllLines(hostsFilePath, savedFile);
         }
-
-        private static void ScanLog()
-        {
-            var directory = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"Low\VRChat\VRChat");
-            if (directory != null && directory.Exists)
-            {
-                FileInfo target = null;
-                foreach (var info in directory.GetFiles("output_log_*.txt", SearchOption.TopDirectoryOnly))
-                {
-                    if (target == null || info.LastWriteTime.CompareTo(target.LastWriteTime) >= 0) target = info;
-                }
-
-                if (target != null)
-                {
-                    Process[] VRCProcs = Process.GetProcessesByName("VRChat");
-                    if (VRCProcs != null && VRCProcs.Length > 0)
-                    {
-                        Logger.Log($"Watching VRChat Process [{target.Name}]");
-                        Process VRChat = VRCProcs[0];
-
-                        ReadNewLines(target.FullName);
-
-                        while (!VRChat.HasExited)
-                        {
-                            ReadLog(target.FullName);
-                            Thread.Sleep(1);
-                        }
-
-                        ToggleHostBlock(false);
-                    }
-                }
-            }
-        }
-
-
-        private static void ReadLog(string Path)
-        {
-            var lines = ReadNewLines(Path);
-
-            foreach (var line in lines)
-            {
-                if (line.Contains("Destination set: "))
-                {
-                    ToggleHostBlock(line.Contains("Destination set: wrld_d319c58a-dcec-47de-b5fc-21200116462c"));
-                }
-
-                else if (line.Contains("OnPlayerJoined "))
-                {
-                    string[] parts = line.Split(new[] { "OnPlayerJoined " }, StringSplitOptions.None);
-                    string DisplayName = parts[1];
-
-                    if (APIClient.MutedUsers.Contains(DisplayName)) Logger.Log($"Muted User {DisplayName} connected");
-                    if (APIClient.AdminUsers.Contains(DisplayName)) Logger.Log($"Admin User {DisplayName} connected");
-                    if (APIClient.BannedUsers.Contains(DisplayName)) Logger.Log($"Banned User {DisplayName} connected");
-                }
-            }
-        }
-
-        private static List<string> ReadNewLines(string filePath)
-        {
-            List<string> lines = new();
-
-            try
-            {
-                using var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-                using var reader = new StreamReader(stream);
-
-                reader.BaseStream.Seek(LastReadOffset, SeekOrigin.Begin);
-
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    lines.Add(line);
-                }
-
-                LastReadOffset = reader.BaseStream.Position;
-            }
-            catch (IOException ex)
-            {
-                Logger.LogError(ex);
-            }
-
-            return lines;
-        }
-
-        private static long LastReadOffset = 0;
     }
 }
--- VRC-AntiFBTHeaven/Wrappers/Logger.cs
+++ VRC-AntiFBTHeaven/Wrappers/Logger.cs
@@ -5,37 +5,37 @@
         public static void Log(object obj)
         {
             Console.ForegroundColor = ConsoleColor.DarkCyan;
-            Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] [AntiFBT] {obj}");
+            Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] [UDONBLOCK] {obj}");
         }
 
         public static void LogDebug(object obj)
         {
             Console.ForegroundColor = ConsoleColor.DarkGray;
-            Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] [AntiFBT] {obj}");
+            Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] [UDONBLOCK] {obj}");
         }
 
         public static void LogImportant(object obj)
         {
             Console.ForegroundColor = ConsoleColor.DarkMagenta;
-            Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] [AntiFBT] {obj}");
+            Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] [UDONBLOCK] {obj}");
         }
 
         public static void LogSuccess(object obj)
         {
             Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] [AntiFBT] {obj}");
+            Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] [UDONBLOCK] {obj}");
         }
 
         public static void LogError(object obj)
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] [AntiFBT] {obj}");
+            Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] [UDONBLOCK] {obj}");
         }
 
         public static void LogWarning(object obj)
         {
             Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] [AntiFBT]  {obj}");
+            Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] [UDONBLOCK]  {obj}");
         }
     }
 }
